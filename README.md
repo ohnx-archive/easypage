884c5fa74a19739695d7696fb33d8e0f2e83f0cf
Mason X <masonx@masonx.tk>
2014-09-01 16:04:34 -0400

    Learning how to use git CLI >.>

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ However, this also offers encryption, and you can edit files.
 <li>Encryption, as in you have to enter a password in order to unlock the contents of the file.</li>
 <li>Edit files, as in there is a way to login as an admin and edit/create the text files.</li>
 </ul>
+Note
+=======
+This was intended mainly for my use, but I put it up in case someone else might find it useful.
+Because of this, customization and other things may be slightly harder, and there is no easy install script.
+If you want me to make one, please create an issue, and I'll attend to it ASAP :)
--- make.php
+++ make.php
@@ -1,5 +1,75 @@
-<title>Voogle</title>
 <?php
-$url="files/"."test".".md";
-echo file_get_contents($url);
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="Please login"');
+    header('HTTP/1.0 401 Unauthorized');
+echo <<<FIN
+<html>
+<head>
+<title>Unauthorized</title>
+<link href='base.css' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<div>
+Sorry, you're not allowed here.
+Please go to <a href="./">here</a>
+</div>
+</body>
+</html>
+FIN;
+exit;
+} else {
+include("settings.php");
+if(true||($user==$_SERVER['PHP_AUTH_USER'])&&($passwd==$_SERVER['PHP_AUTH_PW'])){
+} else {
+echo <<<FIN
+<html>
+<head>
+<title>Unauthorized</title>
+<link href='base.css' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<div>
+Sorry, you're not allowed here.
+Please go to <a href="./">here</a>
+</div>
+</body>
+</html>
+FIN;
+exit;
+}
+}
+//The user has been sucessfully authenticated, continue on to do stuff
+if(
+<html>
+<head>
+<title>Kontrol Panel</title>
+<link href='base.css' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<div>
+<h1>Welcome to the Kontrol Panel</h1>
+<span class="focus">Here you can edit files, and create new ones.</span>
+<h2>Edit existing files</h2>
+<!-- Yeah, I'm actually using a TABLE here, but it's  a logical use for it, in my opinion :3 -->
+<table align="center" style="font-size:1em;">
+<tr style="background-color:gray;color:white;font-style:bold;">
+<td>Filename</td>
+<td>Edit</td>
+<td>Delete</td>
+</tr>
+<?php
+$arr = array_diff(scandir('./files'), array('..', '.'));
+foreach ($arr as $value) {
+    echo "<tr><td>$value</td><td><a href='?option=edit&file=$value'>Edit</a></td><td><a href='?option=delete&file=$value'>Delete</a></td>";
+}
 ?>
+</table>
+<h2>Create a new file</h2>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="GET">
+Filename: <input name="file" type="text"/>.md
+<input name="option" value="edit" type="hidden"/>
+<input type="submit" value="Submit"/>
+</form>
+</div>
+</body>
+</html>
