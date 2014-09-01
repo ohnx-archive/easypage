4bbad0057db27b2a4a219f94c0e0d886a046e2ab
Mason X <masonx@masonx.tk>
2014-09-01 16:48:32 -0400

    Working on make.php, finished deleting procedure

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ Note
 This was intended mainly for my use, but I put it up in case someone else might find it useful.
 Because of this, customization and other things may be slightly harder, and there is no easy install script.
 If you want me to make one, please create an issue, and I'll attend to it ASAP :)
+Known setup requirements:
+If you are on linux, you must chmod the directory "files" to 755, or 777.
+(eg chmod 755 files/ )
+credits
+==========
+crudely uses AlphaCrypt to encrypt/decrypt the files.
+(<a href="http://www.myersdaily.org/joseph/javascript/alphac.html">info here</a>)
--- make.php
+++ make.php
@@ -39,7 +39,7 @@ exit;
 }
 }
 //The user has been sucessfully authenticated, continue on to do stuff
-if(
+?>
 <html>
 <head>
 <title>Kontrol Panel</title>
@@ -49,7 +49,10 @@ if(
 <div>
 <h1>Welcome to the Kontrol Panel</h1>
 <span class="focus">Here you can edit files, and create new ones.</span>
-<h2>Edit existing files</h2>
+<?php
+if(!isset($_REQUEST['option'])){
+?>
+<h2>Edit or delete existing files</h2>
 <!-- Yeah, I'm actually using a TABLE here, but it's  a logical use for it, in my opinion :3 -->
 <table align="center" style="font-size:1em;">
 <tr style="background-color:gray;color:white;font-style:bold;">
@@ -70,6 +73,44 @@ Filename: <input name="file" type="text"/>.md
 <input name="option" value="edit" type="hidden"/>
 <input type="submit" value="Submit"/>
 </form>
+<?php
+}
+else if($_GET['option']=="delete"){
+?>
+<h2>Delete a file</h2>
+<div class="focus">You have requested to delete the file <b><?php echo $_GET['file'];?></b>. Are you sure you want to do this?</p>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+<input name="option" value="delc" type="hidden"/>
+<input name="file" value="<?php echo $_GET['file'];?>" type="hidden"/>
+<input name="hash" value="<?php echo md5($_GET['file']);?>" type="hidden"/>
+<input type="submit" value="Yes"/>
+</form>
+<button onclick="window.history.back()">No</button>
+<?php
+}
+//using POST here to force the user to go through the form, sort of
+else if($_POST['option']=="delc"){
+if(md5($_POST['file'])==$_POST['hash']){
+echo file_get_contents('./files/'.$_POST['file']);
+unlink('./files/'.$_POST['file']);
+echo "Deleted file.";
+} else {
+echo "The file and hash did not match. Please try again.";
+}
+?>
+<a href="<?php echo $_SERVER['PHP_SELF'];?>">Go Back</a>
+<?php
+} else if($_GET['option']=="edit"){
+?>
+
+<?php
+} else if($_POST['option']=="save"){
+?>
+
+
+<?php
+}
+?>
 </div>
 </body>
 </html>
