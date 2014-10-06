42fcd280cf98f24cd41783d8d4c9f2b5916f6237
Mason X <masonx.01@gmail.com>
2014-10-06 16:39:18 +0000

    remove encryption - too hard

--- README.md
+++ README.md
@@ -4,12 +4,6 @@ Make webpages easy
 about
 ========
 What in the world does easypage do? Well, on the most basic level, it takes a file with text formatting(ohnxCode, or oC, for more information see guide.txt), and displays it.
-Simple. In fact, there are already tons of things that do this probably.
-However, this also offers encryption, and you can edit files.
-<ul>
-<li>Encryption, as in you have to enter a password in order to unlock the contents of the file.</li>
-<li>Edit files, as in there is a way to login as an admin and edit/create the text files.</li>
-</ul>
 Note
 =======
 This was intended mainly for my use, but I put it up in case someone else might find it useful.
@@ -18,7 +12,3 @@ If you want me to make one, please create an issue, and I'll attend to it ASAP :
 Known setup requirements:
 If you are on linux, you must chmod the directory "files" to 755, or 777.
 (eg chmod 755 files/ )
-credits
-==========
-crudely uses AlphaCrypt to encrypt/decrypt the files.
-(<a href="http://www.myersdaily.org/joseph/javascript/alphac.html">info here</a>)
--- fetch.php
+++ fetch.php
@@ -59,47 +59,6 @@ $pwd = $_POST['pwd'];
 <head>
 <title><?php echo $id;?></title>
 <link href='base.css' rel='stylesheet' type='text/css' />
-<?php
-if(isset($_POST['pwd'])&&($_POST['usepwd']=="yes")){
-//Begin portion crudely copied from alphacrypt website
-//I really need to learn JavaScript one day, it'll probably help with ajax
-?>
-<script type="text/javascript" src="alphac/alphac.js"></script>
-<script type="text/javascript" src="alphac/md5.js"></script>
-<script type="text/javascript" src="alphac/timer.js"></script>
-<script type="text/javascript">
-function process(on) {
-var fe = document.fe;
-var ke = fe.key.value;
-var re = fe.re;
-var f = timer(function() {
-re.value = alphac(re.value, ke, md5(ke), on)
-});
-var t = f();
-var d = re.value.length;
-}
-</script>
-</head>
-<body onload="process(0)">
-<form action="?" name="fe"
-onsubmit="process(window.a=!window.a); return false">
-<div align="center">
-<textarea rows="12" cols="64" name="re">
-<?php
-$path_to_text="files/".$id.".md";
-echo parse(file_get_contents($path_to_text));
-?>
-</textarea>
-<p>
-Key: <input type="text" name="key" size="24" value="<?php echo $pwd;?>"/>
-</p>
-</div>
-</form>
-<hr />
-Read another:
-<?php
-} else {
-?>
 </head>
 <body>
 <div>
@@ -111,7 +70,6 @@ if($_GET['hide']!="true"){
 <hr />
 Read another:<br />
 <?php
-}
 }
 } else {
 ?>
